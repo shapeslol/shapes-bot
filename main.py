f8d2ea12ed0d34065384cbf4271db1463bad19bb
LeAndre <lcjunior1220@gmail.com>
2025-09-17 23:18:20 -0400

    ee

--- main.py
+++ main.py
@@ -538,8 +538,7 @@ async def robloxinfo(interaction: discord.Interaction, user: str = "Roblox"):
     print(f"Searching For {user}'s profile")
     await interaction.response.defer(thinking=True)
     thinkingembed = discord.Embed(
-                title=f"{interaction.user.name} Searching For {user}'s Roblox Profile!",
-                emoji="<:loading:1416950730094542881>",
+                title=f"<:loading:1416950730094542881>" {interaction.user.name} Searching For {user}'s Roblox Profile!",
                 color=discord.Color.blue()
             )
     await interaction.followup.send(embed=thinkingembed)

6b412808da3e47df655616be4539d2a0207c31c9
LeAndre <187464993+lecanact@users.noreply.github.com>
2025-09-23 18:59:49 -0400

    add .response.

--- main.py
+++ main.py
@@ -659,7 +659,7 @@ async def settings(interaction: discord.Interaction):
 
         @discord.ui.button(label="Change Embed Color", style=discord.ButtonStyle.primary, custom_id="change_embed_color", emoji="🎨")
         async def change_embed_color(self, interaction: discord.Interaction, button: discord.ui.Button):
-            await interaction.send_message(content="Select an embed color from the menu", view=view)
+            await interaction.response.send_message(content="Select an embed color from the menu", view=view)
             #await interaction.response.send_modal(EmbedColorModal())
         #@discord.ui.button(label="Toggle Counting", style=discord.ButtonStyle.primary, custom_id="toggle_counting")
         #async def toggle_counting(self, interaction: discord.Interaction, button: discord.ui.Button):

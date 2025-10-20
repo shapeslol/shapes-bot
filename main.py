429cc2a14cdbd1171a385ffbb225471025a25c17
LeAndre <187464993+lecanact@users.noreply.github.com>
2025-10-20 17:03:19 -0400

    Fix reaction addition for warning condition

--- main.py
+++ main.py
@@ -626,7 +626,7 @@ async def on_message(message):
                 countingDB.save()
                 return
             if warnings >= 3:
-                message.add_reaction('❌️')
+                await message.add_reaction('❌️')
                 if number > HighestNumber:
                     HighestNumber = number
                 counting_data['highestnumber'] = HighestNumber
